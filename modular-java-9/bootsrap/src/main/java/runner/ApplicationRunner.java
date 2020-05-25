614a673ae69bed365f78920975e7cf830d96fd24
shirishphatangare <sphatangare@gmail.com>
2020-05-25 17:15:16 -0500

    Made changes in pom.xml to run application as a Maven Multi-module project on Java 11 using Java Modularity feature

--- modular-java-9/bootsrap/src/main/java/runner/ApplicationRunner.java
+++ modular-java-9/bootsrap/src/main/java/runner/ApplicationRunner.java
@@ -1,18 +1,20 @@
 package runner;
 
-import car.ModularCar;
-import carprocessor.ModularCarProcessor;
-import csv.ModularCSVWriter;
-import json.ModularJSONReader;
-import org.json.simple.parser.ParseException;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
+import org.json.simple.parser.ParseException;
+
+import car.ModularCar;
+import carprocessor.ModularCarProcessor;
+import csv.ModularCSVWriter;
+import json.ModularJSONReader;
+
 public class ApplicationRunner {
     public static void main(String args[]) throws IOException, ParseException {
+    	System.out.println("Initiaiting ApplicationRunner..");
         URL file_path = ApplicationRunner.class.getClassLoader().getResource("cars.json");
 
         ModularJSONReader jsonReader = new ModularJSONReader(file_path.getPath());
@@ -23,6 +25,7 @@ public class ApplicationRunner {
 
         ModularCSVWriter csvWriter = new ModularCSVWriter(new File("cars.csv").getAbsolutePath());
         csvWriter.writeToCSV(fileredList);
+        System.out.println("Terminating ApplicationRunner..");
     }
 }
 
