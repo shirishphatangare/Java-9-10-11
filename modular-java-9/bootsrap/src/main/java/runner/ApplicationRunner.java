5a989f8a6992a6a6de738b6b9d20f569a0bf70e5
shirishphatangare <sphatangare@gmail.com>
2020-05-25 20:08:48 -0500

    Fixed Jar execution issue by using getResourceAsStream() method

--- modular-java-9/bootsrap/src/main/java/runner/ApplicationRunner.java
+++ modular-java-9/bootsrap/src/main/java/runner/ApplicationRunner.java
@@ -2,7 +2,7 @@ package runner;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
 import java.util.List;
 
 import org.json.simple.parser.ParseException;
@@ -15,9 +15,17 @@ import json.ModularJSONReader;
 public class ApplicationRunner {
     public static void main(String args[]) throws IOException, ParseException {
     	System.out.println("Initiaiting ApplicationRunner..");
-        URL file_path = ApplicationRunner.class.getClassLoader().getResource("cars.json");
+        
+    	//Using getResource() method as on below line do not work in case of a jar file
+    	//because cars.json file is not found even though it is present in a jar file
+    	//When the resource files are packaged inside the JAR and so we need a different way of accessing them.
+    	//URL file_path = ApplicationRunner.class.getClassLoader().getResource("cars.json");
+    	
+    	// ClassLoader.getResourceAsStream() looks at the classpath for the given resource.
+    	// The leading slash on the filename to getResourceAsStream() tells the loader to read from the base of the classpath
+    	InputStream inputStream = ApplicationRunner.class.getResourceAsStream("/cars.json");
 
-        ModularJSONReader jsonReader = new ModularJSONReader(file_path.getPath());
+        ModularJSONReader jsonReader = new ModularJSONReader(inputStream);
         List<ModularCar> cars = jsonReader.parseFile();
 
         ModularCarProcessor modularCarProcessor = new ModularCarProcessor(cars);
@@ -29,3 +37,25 @@ public class ApplicationRunner {
     }
 }
 
+/*Notes: Let's instead use resource loading to load resources from the classpath instead of a specific file location. This will work regardless of how the code is packaged:
+
+// SUCCESS case:
+ 
+	try (InputStream inputStream = getClass().getResourceAsStream("/input.txt");
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+	    String contents = reader.lines()
+	      .collect(Collectors.joining(System.lineSeparator()));
+	}
+	
+	ClassLoader.getResourceAsStream() looks at the classpath for the given resource. The leading slash on the input to getResourceAsStream() tells the loader to read from the base of the classpath. The contents of our JAR file are on the classpath, so this method works.
+	An IDE typically includes src/main/resources on its classpath and, thus, finds the files. */
+
+
+/*FAILURE Case:
+	
+	try (FileReader fileReader = new FileReader("src/main/resources/input.txt"); 
+		     BufferedReader reader = new BufferedReader(fileReader)) {
+		    String contents = reader.lines()
+		      .collect(Collectors.joining(System.lineSeparator()));
+		}
+*/
--- modular-java-9/file-reader/src/main/java/json/ModularJSONReader.java
+++ modular-java-9/file-reader/src/main/java/json/ModularJSONReader.java
@@ -1,26 +1,35 @@
 package json;
 
-import car.ModularCar;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
+import car.ModularCar;
 
 public class ModularJSONReader {
-    private final String targetFilePath;
+    //private final String targetFilePath;
+	private InputStream inputStream ;
 
-    public ModularJSONReader(String targetFilePath) {
-        this.targetFilePath = targetFilePath;
-    }
+//    public ModularJSONReader(String targetFilePath) {
+//        this.targetFilePath = targetFilePath;
+//    }
+	
+	  public ModularJSONReader(InputStream inputStream) {
+		  this.inputStream = inputStream;
+	  }
 
     public List<ModularCar> parseFile() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
-        JSONObject json = (JSONObject) parser.parse(new FileReader(targetFilePath));
+        //JSONObject json = (JSONObject) parser.parse(new FileReader(targetFilePath));
+        JSONObject json = (JSONObject) parser.parse(new BufferedReader(new InputStreamReader(inputStream)));
         JSONArray cars = (JSONArray) json.get("cars");
 
         List<JSONObject> carList = (List<JSONObject>) cars.stream().collect(Collectors.toList());
